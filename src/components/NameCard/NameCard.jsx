0b24b127cb9102bb4f673afb6819b7cc68cdfc92
Prasad Rathod <2580prasadrathod@gmail.com>
2021-05-02 08:49:22 +0530

    Add : noreferrer to a tags

--- src/components/NameCard/NameCard.jsx
+++ src/components/NameCard/NameCard.jsx
@@ -4,7 +4,7 @@ import './NameCard.css'
 const nameCheapURL = 'https://www.namecheap.com/domains/registration/results/?domain='
 const NameCard = ({ suggestionName }) => {
     return (
-        <a className="card-link" href={`${nameCheapURL}${suggestionName}`} target="_blank">
+        <a rel="noreferrer" className="card-link" href={`${nameCheapURL}${suggestionName}`} target="_blank">
             <div className="result-name-card">
                 <p className="result-name">{suggestionName}</p>
             </div>
